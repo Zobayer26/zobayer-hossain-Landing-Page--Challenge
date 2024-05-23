aeefee7cef27ae4ed4b2f42cc8009f6ad38b7d1e
Zobayer26 <zobayerarif126@gmail.com>
2024-05-23 19:29:57 +0600

    About part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import About_img from "../assets/About _img.png"
 
 const About = () => {
   return (
-    <section className="mt-[150px] flex">
-      <div className=" py-[126px] pl-[245px] pr-[165px] bg-[#FED29C]  ">
-        <div className="w-[541px]">
-          <h1 className="text-[#383838] text-[38px] leading-[48px] font-[700] mb-[20px]">
+    <section className="mt-[150px] flex flex-col 2xl:flex-row ">
+      <div className="px-[90px] py-[60px] xl:py-[126px] xl:pl-[245px] xl:pr-[165px] bg-[#FED29C]  ">
+        <div className="max-w-[541px]">
+          <h1 className="text-[#383838] text-[30px] md:text-[38px] leading-[48px] font-[700] mb-[20px]">
             Experience clear
             vision with OptiChroma Vision Frames
           </h1>
-          <div className="text-[20px] leading-[30px] font-[400] text-[#545454]">
+          <div className="text-[18px] leading-[24px]  md:text-[20px] md:leading-[30px] font-[400] text-[#545454]">
             <p className=" mb-[10px]">
               At OptiChroma, we believe that everyone deserves
               clear vision and stylish eyewear. That&apos;s why we&apos;ve
@@ -24,7 +24,7 @@ const About = () => {
                   text-[18px] leading-[26px] font-[700]'>about us
         </button>
       </div>
-      <div>
+      <div className="hidden md:block h-[616px] 2xl:w-[850px]">
         <img src={About_img} alt="" className="h-full w-full object-cover"/>
       </div>
     </section>
